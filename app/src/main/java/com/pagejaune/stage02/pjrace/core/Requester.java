0dab3d04ce5fd949c793e2355ec88383515918a4
STAGE  02 <stage02@pt-dev12.bed1t.local>
2017-02-06 10:48:00 +0100

    Initial commit

---
+++ app/src/main/java/com/pagejaune/stage02/pjrace/core/Requester.java
@@ -0,0 +1,32 @@
+package com.pagejaune.stage02.pjrace.core;
+
+import com.pagejaune.stage02.pjrace.network.Request;
+
+/**
+ * Created by stage02 on 06/02/17.
+ */
+
+public class Requester implements RequestCallBack {
+
+    public Requester(){
+
+    }
+
+    @Override
+    public void onSuccess() {
+
+    }
+
+    @Override
+    public void onFailure() {
+
+    }
+
+    public void startMapRequest(){
+
+    }
+
+    public void setMapRequestListener(){
+
+    }
+}
